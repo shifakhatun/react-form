d51ec8643b7753368d4bfb9d9731207798c4093a
shifakhatun <shifa.eee@gmail.com>
2024-03-19 03:16:10 +0600

    modal ad required added

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -6,13 +6,20 @@ const Registration = () => {
     const[name, setName]=useState("");
     const[email, setEmail]=useState("");
     const[password, setPassword]=useState("");
+    const[isSubmit, setIsSubmit]=useState(false);
+    const [ischecked, setChecked]=useState(false);
 
     const handleSubmit=(event)=>{
         event.preventDefault();
+        if(!ischecked){
+            console.log(alert("try again"));
+            return;
+        } 
        const  user = {
         Name:name, Email:email, Password: password
        };
        console.log(user);
+       setIsSubmit(true);
     };
 
     
@@ -30,28 +37,46 @@ const Registration = () => {
             onChange={(event)=> setName(event.target.value)}
             // onChange={(event)=> console.log(event.target.value)}
             type="text" placeholder="Enter your name"
-            className="py-2 px-1 border-2 w-full rounded"/>
+            className="py-2 px-1 border-2 w-full rounded"
+            required
+            />
 
             <label className="mb-2 text-lg">Email</label>
             <input 
             onChange={(event)=> setEmail(event.target.value)}
             type="email" placeholder="Enter your email"
-            className="py-2 px-1 border-2 w-full rounded"/>
+            className="py-2 px-1 border-2 w-full rounded"
+            required
+            />
 
             <label className="mb-2 text-lg">Password</label>
             <input 
             onChange={(event)=> setPassword(event.target.value)}
             type="password" placeholder="Enter your password"
-            className="py-2 px-1 border-2 w-full rounded"/>
+            className="py-2 px-1 border-2 w-full rounded"
+            required
+            />
 
-
+              <input type="checkbox" onChange={(event)=>setChecked(event.target.checked)} /> do yu agree with terms and condition
 
             <div className="flex justify-end mt-3">
                 <button type="submit" className="bg-purple-500 h-12 w-20 text-xl text-white">
                     SignUp
                     </button>
             </div>
-            </form>                
+            </form>    
+
+            {
+                isSubmit && <div className=" fixed top-0 flex justify-center items-center h-screen w-full bg-purple-500">
+                <div className="w-1/2 h-1/2 rounded bg-purple-300 text-center " >
+                   <h1 className="text-3xl mb-6 text-purple-500">you have submitted the form successfully
+                     </h1>
+                     <p className="text-xl">Name: {name} </p>
+
+
+                </div>
+                </div>    
+            }        
             </div>
         </div>
     );
